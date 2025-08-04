3e05db7a289d4589c7561c6180158d917d5af7f1
AwesDes <furiousmaster18@gmail.com>
2025-08-05 02:42:42 +0800

    a

--- out/html/game.js
+++ out/html/game.js
@@ -41,6 +41,14 @@
     }
   };
 
+  window.showEconomy = function() {
+    if (window.dendryUI.dendryEngine.state.sceneId.startsWith('rev_economy')) {
+        window.dendryUI.dendryEngine.goToScene('backSpecialScene');
+    } else {
+        window.dendryUI.dendryEngine.goToScene('rev_economy');
+    }
+  };
+
   window.showMods = function() {
     window.hideOptions();
     if (window.dendryUI.dendryEngine.state.sceneId.startsWith('mod_loader')) {
