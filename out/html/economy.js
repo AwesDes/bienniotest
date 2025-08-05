3761deabdc35d8abd810edc120f502bf0fbb04b2
AwesDes <furiousmaster18@gmail.com>
2025-08-05 10:59:57 +0800

    a

---
+++ out/html/economy.js
@@ -0,0 +1,15 @@
+document.getElementById('path3270').addEventListener('click', function() {
+    console.log("Pressed Region A")
+});
+
+document.getElementById('path3300').addEventListener('click', function() {
+    console.log("Pressed Region B")
+});
+
+document.getElementById('path3312').addEventListener('click', function() {
+    console.log("Pressed Region C")
+});
+
+document.getElementById('path3316').addEventListener('click', function() {
+    console.log("Pressed Region D")
+});
