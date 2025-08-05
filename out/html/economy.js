8ac8fe943c6ed487f68171e09b97c087e2c8c2af
AwesDes <furiousmaster18@gmail.com>
2025-08-05 11:26:09 +0800

    a

--- out/html/economy.js
+++ out/html/economy.js
@@ -1,15 +1,28 @@
 document.getElementById('path3270').addEventListener('click', function() {
-    console.log("Pressed Region A")
+    document.getElementById('southern-economy').style.display = "";
+    document.getElementById('central-economy').style.display = "none";
+    document.getElementById('northwest-economy').style.display = "none";
+    document.getElementById('northeast-economy').style.display = "none";
 });
 
 document.getElementById('path3300').addEventListener('click', function() {
-    console.log("Pressed Region B")
+    document.getElementById('central-economy').style.display = "";
+    document.getElementById('southern-economy').style.display = "none";
+    document.getElementById('northwest-economy').style.display = "none";
+    document.getElementById('northeast-economy').style.display = "none";
 });
 
 document.getElementById('path3312').addEventListener('click', function() {
-    console.log("Pressed Region C")
+    document.getElementById('northwest-economy').style.display = "";
+    document.getElementById('southern-economy').style.display = "none";
+    document.getElementById('central-economy').style.display = "none";
+    document.getElementById('northeast-economy').style.display = "none";
+
 });
 
 document.getElementById('path3316').addEventListener('click', function() {
-    console.log("Pressed Region D")
+    document.getElementById('northeast-economy').style.display = "";
+    document.getElementById('southern-economy').style.display = "none";
+    document.getElementById('central-economy').style.display = "none";
+    document.getElementById('northwest-economy').style.display = "none";
 });
